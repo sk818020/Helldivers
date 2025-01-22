bc866d5533c292da20bf9df2a10a536afa64eb93
sk818020 <53592941+sk818020@users.noreply.github.com>
2025-01-21 20:23:48 -0700

    Added filters and note.

--- Helldiver_main.py
+++ Helldiver_main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from seaborn.external.docscrape import header
 from st_aggrid import AgGrid
 import st_aggrid
 import json
@@ -9,12 +10,14 @@ import matplotlib.pyplot as plt
 from streamlit_plotly_events import plotly_events
 import plotly.express as px
 import datetime as dt
+from streamlit_dynamic_filters import DynamicFilters
 
 st.set_page_config(layout="wide")
 
 res = requests.get("https://helldiverstrainingmanual.com/api/v1/war/campaign")
 response = json.loads(res.text)
 df = pd.DataFrame(response)
+df.rename(columns={'players': 'player_count'}, inplace=True)
 l_planets = df['planetIndex'].unique().tolist()
 df3 = pd.DataFrame()
 for i in l_planets:
@@ -30,32 +33,46 @@ map1 = df[['planetIndex', 'name', 'faction']]
 df3 = pd.merge(df3, map1, left_on='planet_index', right_on='planetIndex')
 df3['created_at'] = pd.to_datetime(df3['created_at'])
 df3['created_at'] = df3['created_at'].dt.tz_convert('MST')
-
-
+#st_aggrid.AgGrid(df)
 stamp = dt.datetime.now().strftime('%m/%d/%y - %H:%M')
 st.title('Helldivers 2 Player Count Analysis - {x} MST'.format(x = stamp))
 st.text_area("",
-    "Note: the left graph uses minute by minute data, and the right uses every 5 minute minute data. This can lead to small differences"\
+    "Note: the left graph uses minute by minute data, and the right uses every 5 minute data. This can lead to small differences"\
     " in player count.",
              height = 68
 )
-fig1 = px.bar(df, x='faction', y='players', color = 'name',
-              title = 'Player Count by Faction and Planet')
-fig2 = px.line(df3, x = 'created_at', y = 'player_count', color = 'name',
-               title = 'Player Count by Planet and Time (last 24 hours, Mountain time)')
-
 st.sidebar.header('Choose selection:')
-planet_filter = st.sidebar.selectbox(
+planet_filter = st.sidebar.multiselect(
     'Select Planet',
     options = df3['name'].unique()
 )
-faction_filter = st.sidebar.selectbox(
+faction_filter = st.sidebar.multiselect(
     'Select Faction',
-    options = df3['faction'].unique()
+    options = df3['faction'].unique(),
+    default = df3['faction'].unique()
 )
+
 col1, col2 = st.columns(2)
+fig1 = px.bar(df[df['faction'].isin(faction_filter)], x='faction', y='player_count', color = 'name',
+              title = 'Player Count by Faction and Planet')
+fig2 = px.line(df3[df3['faction'].isin(faction_filter)], x = 'created_at', y = 'player_count', color = 'name',
+               title = 'Player Count by Planet and Time (last 24 hours, Mountain time)')
+
+
 with col1:
     st.plotly_chart(fig1)
 with col2:
     st.plotly_chart(fig2)
-st_aggrid.AgGrid(df3)
+@st.cache_data
+def convert_df(df3):
+    return df3.to_csv(index=False).encode('utf-8')
+
+csv = convert_df(df3)
+st.download_button(
+    "Press to download data",
+    csv,
+    "file.csv",
+    "text/csv",
+    key="download-csv"
+)
+st_aggrid.AgGrid(df3[df3['faction'].isin(faction_filter)])
